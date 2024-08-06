8ffaf52bf1867d2bd827bddfc0a3c2da5da0a717
Iamtrieves <indokdavid2020@gmail.com>
2024-08-06 00:07:30 -1200

    RWD done

---
+++ script.js
@@ -0,0 +1,60 @@
+function enableFixedNavigationBar() {
+    const navBar = document.getElementById('nav-bar');
+    const isScrolledPastThreshold = window.scrollY >= 49;
+
+    // Toggle fixed navbar and box-shadow based on scroll position
+    navBar.classList.toggle("fixed-nav-bar", !isScrolledPastThreshold);
+    navBar.classList.toggle("nav-bar-box-shadow", isScrolledPastThreshold);
+}
+
+function setLinkColor(color) {
+    const links = document.querySelectorAll('.nav-link');
+    links.forEach(link => {
+    link.style.color = color;
+    link.style.border.bottom = "1px solid var(--tekins-green)";
+});
+}
+
+// Event Listeners
+window.addEventListener('scroll', () => {
+    if (!navMenu.classList.contains("active") && window.scrollY < 49) {
+    setLinkColor('var(--tekins-white)');
+    } else {
+        setLinkColor('var(--tekins-green');
+    }
+    enableFixedNavigationBar();
+});
+
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-links");
+const body = document.querySelector('#body');
+
+hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+
+    // Update link color based on navMenu state
+    setLinkColor(navMenu.classList.contains("active") ? 'var(--tekins-green)' : 'var(--tekins-white)');
+
+    // Prevent scrolling when navMenu is active
+    document.body.classList.toggle("no-scroll", navMenu.classList.contains("active"));
+});
+
+// Targets the hamburger & navMenu variable and removes the class active attached to them i.e closes by clicking any of them or the links it takes away the navMenu from the page
+document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () => {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+}))
+// Targets the the body element and removes the class no-scroll on the it when any of the nav-link is clicked
+document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () => {
+    body.classList.remove("no-scroll");
+}))
+
+
+// Current Year 
+const displayCurrentYear = () => {
+    const currentYearElement = document.querySelector('.current-year');
+    const actualCurrentYear = new Date().getFullYear();
+    currentYearElement.textContent = actualCurrentYear;
+};
+displayCurrentYear(); 
